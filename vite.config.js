a0755e7112568af5ffdae70d6ca4b076c763d569
Vansh Nazwani <nazwaniv@gmail.com>
2025-11-16 23:40:17 +0530

    Files pushed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/PORTFOLIO/",       // <----- REQUIRED for GitHub Pages
+  base: "/",       // THIS MUST WORK
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
