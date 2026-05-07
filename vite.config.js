8528ad0ac2d49bad93b910d2c1c6991cee5818ca
VanshNazwani <nazwaniv@gmail.com>
2026-05-07 18:46:42 +0000

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/PORTFOLIO/",
+  base: '/portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
